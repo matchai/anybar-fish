0f3472e01b77edc0b1269bd0ea5612f1547fb948
Matan Kushner <hello@matchai.me>
2018-11-15 21:37:57 -0500

    Fix completion errors

--- completions/anybar.fish
+++ completions/anybar.fish
@@ -2,5 +2,5 @@ set -l styles white red orange yellow green cyan blue purple black question excl
 
 for style in $styles
   echo "entry: $style"
-  complete -xc anybar -a "$style" -d "Set the Anybar dot to the $style style" -a 
+  complete -xc anybar -n "__fish_use_subcommand" -a "$style" -d "Set the Anybar dot to the $style style"
 end
