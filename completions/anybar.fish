0fe52e55e6b6465fdfca5414a450776d53de37f5
Matthew Erker <matterker@users.noreply.github.com>
2021-02-23 19:13:18 -0600

    Add completion for quit command (#5)

--- completions/anybar.fish
+++ completions/anybar.fish
@@ -4,3 +4,5 @@ for style in $styles
   echo "entry: $style"
   complete -xc anybar -n "__fish_use_subcommand" -a "$style" -d "Set the Anybar dot to the $style style"
 end
+
+complete -xc anybar -n "__fish_use_subcommand" -a "quit" -d "Quit AnyBar"
